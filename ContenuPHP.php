353dc45b086bdd3e6b273cb20533411cf5bd9633
romainlmrtl <romain.lemartinel@sts-sio-caen.info>
2021-10-09 20:59:11 +0200

    formouai

---
+++ ContenuPHP.php
@@ -0,0 +1,7 @@
+<h1>Contenu du POST</h1>
+<ul>
+    <li><?=$_POST["prenom"]?></li>
+    <li><?=$_POST["nom-f"]?></li>
+    <li><?=$_POST["email"]?></li>
+    <li><?=$_POST["commentaire"]?></li>
+</ul>
